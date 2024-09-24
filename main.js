27c78b3dc2d56012d3f30f0c721c1ec9d1d95476
isabela <isabela@hubcred.com.br>
2024-09-24 11:28:08 -0300

    geral chat 1

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 document.getElementById("chatbotBtn").addEventListener("click", function() {
-    // Pegando o contêiner onde o chatbot será inserido
-    var chatContainer = document.getElementById("chatContainer");
+  // Pegando o contêiner onde o chatbot será inserido
+  var chatContainer = document.getElementById("chatContainer");
 
-    // Verificando se o chatbot já está inserido, para não adicionar múltiplas vezes
-    if (!chatContainer.innerHTML) {
-      chatContainer.innerHTML = '<iframe allow="microphone;" width="350" height="430" src="https://console.dialogflow.com/api-client/demo/embedded/YOUR_AGENT_ID"></iframe>';
-    }
+  // Verificando se o chatbot já está inserido, para não adicionar múltiplas vezes
+  if (!chatContainer.innerHTML) {
+      chatContainer.innerHTML = '<iframe allow="microphone;" width="350" height="430" src="https://console.dialogflow.com/api-client/demo/embedded/mvp-ceap-v1-bbyk"></iframe>';
+  }
 
-    // Exibindo o contêiner
-    chatContainer.style.display = "block";
-  });
+  // Exibindo o contêiner
+  chatContainer.style.display = "block";
+});
